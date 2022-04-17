19603eb64f946af6f02c9ca8512150ac97a89f5a
mster <michaelsterpka@gmail.com>
2022-04-16 20:07:25 -0700

    v1.0.0: add tests, handle fully transparent imgs, update docs

--- index.js
+++ index.js
@@ -1,78 +1,81 @@
 'use strict'
 
 const path = require('path')
-const { promisify, debuglog } = require('util')
-const readFile = promisify(require('fs').readFile)
+const { readFile } = require('fs/promises')
 const decode = require('image-decode')
-
-const debug = debuglog('domcolor')
 
 module.exports = domcolor
 
-async function domcolor (args) {
-  if (args == null) {
-    debug('args is undefined or null')
-    throw new Error('args is a required parameter')
+/**
+ * Determines the dominant color in an image
+ * @param {String|Buffer} img Image buffer or string path to image
+ * @returns {Object} dominant color data
+ */
+async function domcolor (img) {
+  if (img == null) {
+    throw new Error('Error: img is a required parameter')
   }
 
-  if (typeof args === 'string') {
-    debug('read is string, checking filename')
-
-    const filename = path.basename(args)
-    if (
-      !(
-        filename.length < 256 &&
-        (/^[\w\-. ]+.jpg$/.test(filename) ||
-          /^[\w\-. ]+.jpeg$/.test(filename) ||
-          /^[\w\-. ]+.png$/.test(filename) ||
-          /^[\w\-. ]+.bmp$/.test(filename) ||
-          /^[\w\-. ]+.tiff$/.test(filename) ||
-          /^[\w\-. ]+.gif$/.test(filename))
-      )
-    ) {
-      throw new Error(`Invalid filename: ${filename} at ${args}`)
-    }
+  if (typeof img === 'string') {
+    const filename = path.basename(img)
+    validateFileType(filename)
   }
 
   const imgSrc =
-    args.constructor.name === 'Buffer' ? args : await readFile(args)
+  img.constructor.name === 'Buffer' ? img : await readFile(img)
   const srcData = decode(imgSrc)
 
-  if (!srcData) {
-    debug('invalid args -- got bad buffer')
-    throw new Error('Invalid image source, unable to decode')
+  if (!srcData?.data) {
+    throw new Error('Error: Invalid image source; unable to decode')
   }
 
-  return new Promise((resolve, reject) => {
-    const { data } = srcData
+  const { data } = srcData
 
-    const COLOR_MAP = {}
+  const COLOR_MAP = {}
+  for (let i = 0; i < data.length; i += 4) {
+    /* don't count trasparent pixels, it's boring */
+    if (data[i + 3] === 0) continue
 
-    for (let i = 0; i < data.length; i += 4) {
-      /* don't count trasparent pixels, it's boring */
-      if (data[i + 3] === 0) continue
+    const color = `${data[i + 0]}+${data[i + 1]}+${data[i + 2]}`
+    if (COLOR_MAP[color]) COLOR_MAP[color] = COLOR_MAP[color] + 1
+    else COLOR_MAP[color] = 1
+  }
 
-      const color = `${data[i + 0]}+${data[i + 1]}+${data[i + 2]}`
-      if (COLOR_MAP[color]) COLOR_MAP[color] = COLOR_MAP[color] + 1
-      else COLOR_MAP[color] = 1
-    }
+  const colorKeys = Object.keys(COLOR_MAP)
 
-    const _rgb = Object.keys(COLOR_MAP).reduce((f, g) =>
-      COLOR_MAP[f] > COLOR_MAP[g] ? f : g
-    )
+  // nothing to sort
+  if (!colorKeys?.length) return
 
-    const rgb = _rgb.split('+').map(e => +e)
-    const hex = toHex(rgb)
-    const count = COLOR_MAP[_rgb]
-    const total = Math.round(data.length / 4)
+  const _rgb = colorKeys.reduce((f, g) =>
+    COLOR_MAP[f] > COLOR_MAP[g] ? f : g
+  )
 
-    debug('resolving with data')
-    resolve({ rgb, hex, count, total })
-  })
+  const rgb = _rgb.split('+').map(e => +e)
+  const hex = toHex(rgb)
+  const count = COLOR_MAP[_rgb]
+  const total = Math.round(data.length / 4)
+
+  return { rgb, hex, count, total }
 
   function toHex (rgb) {
     rgb = rgb.map(e => e.toString(16))
     rgb = rgb.map(e => (e.length > 1 ? '' : '0') + e)
-    return `#${rgb.join('')}`
+    return `${rgb.join('')}`
+  }
+}
+
+const validateFileType = (filename) => {
+  if (
+    !(
+      filename.length < 256 &&
+      (/^[\w\-. ]+.jpg$/.test(filename) ||
+        /^[\w\-. ]+.jpeg$/.test(filename) ||
+        /^[\w\-. ]+.png$/.test(filename) ||
+        /^[\w\-. ]+.bmp$/.test(filename) ||
+        /^[\w\-. ]+.tiff$/.test(filename) ||
+        /^[\w\-. ]+.gif$/.test(filename))
+    )
+  ) {
+    throw new Error(`Error: Invalid filename: ${filename}`)
   }
 }
